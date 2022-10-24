8ab5704b00d848686e7337499a41dc9278cd1a92
KrissiTekkari <dadasonk@gmail.com>
2022-10-24 16:38:24 +0000

    5.2 done

--- assignment_5.2_Communication_Rpi_Arduino/arduino_RPi_comm_files/src/main.cpp
+++ assignment_5.2_Communication_Rpi_Arduino/arduino_RPi_comm_files/src/main.cpp
@@ -10,7 +10,39 @@ void setup() {            // called once on start up
    pinMode(ledPin, OUTPUT);         // the LED is an output
 }
 
+
+// part 3
 void loop() {              // loops forever
+   uint8_t command;
+   uint8_t level;
+   char buffer[100];       // stores the return buffer on each loop   
+   if (Serial.available()>0){                 // bytes received
+      Serial.readBytes(buffer, 2); // read 2 bytes
+      command = buffer[0];           // first byte is command
+                                    // if first byte is 2 then set the brightness of the LED
+      if(command==2){   // if first byte is 2 then set the brightness of the LED
+         level = buffer[1] ;    // extract the LED brightness level from buffer
+         // is it in range?
+         // it can't really be out of range because the level is a uint8_t (unsigned)
+         // so if you send a value greater than 255 it will wrap around to 0
+         // or if you send a value less than 0 it will wrap around to 255
+         if(level>=0 && level<=255){          
+            analogWrite(ledPin, level);       // yes, write out
+            sprintf(buffer, "Set brightness to %d", level);
+         }
+         else{                                // no, error message back
+            sprintf(buffer, "Error: %d is out of range", level);
+         } 
+      }                                       // otherwise, unknown cmd
+      else{ sprintf(buffer, "Unknown command: %d", command); }
+      Serial.print(buffer);               // send the buffer to the RPi
+   }
+}
+
+
+// part 2
+
+/* void loop() {              // loops forever
    String command;
    char buffer[100];       // stores the return buffer on each loop   
    if (Serial.available()>0){                 // bytes received
@@ -20,13 +52,13 @@ void loop() {              // loops forever
          int level = intString.toInt();       // extract the int
          if(level>=0 && level<=255){          // is it in range?
             analogWrite(ledPin, level);       // yes, write out
-            sprintf(buffer, "Set brightness to %d", level);
+            sprintf(buffer, "Set brightness to %d\n", level);
          }
          else{                                // no, error message back
-            sprintf(buffer, "Error: %d is out of range", level);
+            sprintf(buffer, "Error: %d is out of range\n", level);
          } 
       }                                       // otherwise, unknown cmd
-      else{ sprintf(buffer, "Unknown command: %s", command.c_str()); }
+      else{ sprintf(buffer, "Unknown command: %s\n", command.c_str()); }
       Serial.print(buffer);               // send the buffer to the RPi
    }
-}
+} */
